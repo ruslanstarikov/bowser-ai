18a07af566d76c23fddf76896bf003739996ce89
Ruslan Starikov <ruslan@beef-burger.com>
2025-03-18 22:51:34 +1100

    Wrinkles

--- src/resources/views/code/ToolTemplate.blade.php
+++ src/resources/views/code/ToolTemplate.blade.php
@@ -1,14 +1,14 @@
-<?php
-
+{!! '<?php' !!}
 namespace App\Tools;
 
 class {{ $class_name }}
 {
     public const NAME = '{{ $class_name }}';
-    public const DESCRIPTION = 'Description for {{ $class_name }} tool. Update this.';
+    public const DESCRIPTION = 'Description for {{ $class_name }} tool. The description is important as it is used to instruct
+	the LLM. Update it, including detailed description for every parameter';
 
-    //
-    public static function run()
+    // Make sure every parameter is a typed primitive. Return is always a string
+    public static function run(): string
     {
         // Implement the tool logic here
     }
